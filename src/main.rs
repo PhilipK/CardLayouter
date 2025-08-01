7ac4a7d8814202f47d25458c0f104c2e8373767c
Philip Kristoffersen <philipkristoffersen@gmail.com>
2025-08-01 20:36:33 +0200

    Draw guidelines

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,20 @@ fn main() {
             }
         }
     }
+    let layout = LayoutSettings::default();
+    let l = &layout;
     let dpi = 300.0;
 
-    let margin = Mm(10.0).into_pt();
-    let card_width = Mm(63.0).into_pt();
-    let card_height = Mm(88.0).into_pt();
+    let target_width_pixels = l.card_width.into_px(dpi);
 
-    let target_width_pixels = card_width.into_px(dpi);
-
-    let target_height_pixels = card_height.into_px(dpi);
+    let target_height_pixels = l.card_height.into_px(dpi);
 
     let mut cards_on_page = 0;
-    for  loaded_image in loaded_images.iter().enumerate() {
+    for loaded_image in loaded_images.iter() {
         let scale_x = target_width_pixels.0 as f32 / loaded_image.width as f32;
         let scale_y = target_height_pixels.0 as f32 / loaded_image.height as f32;
-        let col = card_on_page  / 3;
-        let row = card_on_page % 3;
+        let col = cards_on_page % l.card_columns;
+        let row = cards_on_page / l.card_rows;
         // Add the image to the document resources and get its ID
         let image_id = doc.add_image(&loaded_image);
 
@@ -42,8 +40,8 @@ fn main() {
         page_ops.push(Op::UseXobject {
             id: image_id.clone(),
             transform: XObjectTransform {
-                translate_x: Some((card_width * (col as f32)) + margin),
-                translate_y: Some((card_height * (row as f32)) + margin),
+                translate_x: Some((l.card_width * (col as f32)) + l.margin_x),
+                translate_y: Some((l.card_height * (row as f32)) + l.margin_y),
                 scale_x: Some(scale_x),
                 scale_y: Some(scale_y),
                 dpi: Some(dpi),
@@ -59,7 +57,8 @@ fn main() {
     }
 
     let mut final_pages = vec![];
-    for page_ops  in pages {
+    for mut page_ops in pages {
+        page_ops.extend_from_slice(&draw_lines(l));
         let page = PdfPage::new(Mm(210.0), Mm(297.0), page_ops);
         final_pages.push(page);
     }
@@ -70,6 +69,58 @@ fn main() {
 
     std::fs::write("./image_example.pdf", bytes).unwrap();
     println!("Created image_example.pdf");
+}
+
+fn draw_lines(layout: &LayoutSettings) -> Vec<Op> {
+    //Make this better later
+    let zero = Mm(0.0).into_pt();
+    let l = layout;
+
+    let mut lines = vec![];
+
+    for column in 0..=l.card_columns {
+        lines.push((
+            (l.margin_x + Pt(column as f32 * l.card_width.0), l.margin_y),
+            (l.margin_x + Pt(column as f32 * l.card_width.0), zero),
+        ));
+        lines.push((
+            (l.margin_x + Pt(column as f32 * l.card_width.0), l.page_height - l.margin_y),
+            (l.margin_x + Pt(column as f32 * l.card_width.0), l.page_height ),
+        ));
+    }
+    for row in 0..=l.card_rows {
+        lines.push((
+            (zero, l.margin_y + Pt(row as f32 * l.card_height.0)),
+            (l.margin_x, l.margin_y + Pt(row as f32 * l.card_height.0)),
+        ));
+        lines.push((
+            (l.page_width, l.margin_y + Pt(row as f32 * l.card_height.0)),
+            (l.page_width-l.margin_x, l.margin_y + Pt(row as f32 * l.card_height.0)),
+        ));
+        
+    }
+
+    let mut ops = vec![];
+
+    for (p1, p2) in lines {
+        ops.push(Op::DrawLine {
+            line: Line {
+                points: vec![
+                    LinePoint {
+                        p: Point { x: p1.0, y: p1.1 },
+                        bezier: false,
+                    },
+                    LinePoint {
+                        p: Point { x: p2.0, y: p2.1 },
+                        bezier: false,
+                    },
+                ],
+                is_closed: false,
+            },
+        });
+    }
+
+    ops
 }
 
 fn get_all_pngs(folder_path: &Path) -> Vec<PathBuf> {
@@ -84,4 +135,40 @@ fn get_all_pngs(folder_path: &Path) -> Vec<PathBuf> {
         }
     }
     pngs
+}
+
+struct LayoutSettings {
+    card_width: Pt,
+    card_height: Pt,
+    page_width: Pt,
+    page_height: Pt,
+    margin_x: Pt,
+    margin_y: Pt,
+    card_rows: usize,
+    card_columns: usize,
+}
+
+impl Default for LayoutSettings {
+    fn default() -> Self {
+        let card_width = Mm(63.0).into_pt();
+        let card_height = Mm(88.0).into_pt();
+        let page_width = Mm(210.0).into_pt();
+        let page_height = Mm(297.0).into_pt();
+        let card_columns = (page_width.0 / card_width.0).floor();
+        let margin_x = Pt((page_width.0 - (card_columns * card_width.0)) / 2.0);
+
+        let card_rows = (page_height.0 / card_height.0).floor();
+        let margin_y = Pt((page_height.0 - (card_rows * card_height.0)) / 2.0);
+
+        LayoutSettings {
+            card_width,
+            card_height,
+            page_width,
+            page_height,
+            margin_x,
+            margin_y,
+            card_columns: card_columns as usize,
+            card_rows: card_rows as usize,
+        }
+    }
 }
